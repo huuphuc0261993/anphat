0efe78da04ac06a9b9e5a835be5471e31588a3f7
phuc <you@example.com>
2020-12-08 09:41:46 +0700

    upload

--- app/controllers/api/banners_controller.rb
+++ app/controllers/api/banners_controller.rb
@@ -3,41 +3,41 @@ module Api
     def index
         @banners = Banner.all
         render json: @banners
-        end
-    
-        def create
+    end
+
+    def create
         @banners = Banner.new(banners_params)
         if @banners.save
             render json: { status: :ok, message: 'Success' }
         else
             render json: { json: @banners.errors, status: :unprocessable_entity }
         end
-        end
-    
-        def show
+    end
+
+    def show
         @banners = Banner.find(params[:id])
         render json: { data: @banners, status: :ok, message: 'Success' }
-        end
-    
-        def update
+    end
+
+    def update
         @banners = Banner.find(params[:id])
         if @banners.update(banners_params)
             render json: { status: :ok, message: 'Success' }
         else
             render json: { json: @banners.error, status: :unprocessable_entity }
         end
-        end
-    
-        def destroy
+    end
+
+    def destroy
         @banners = Banner.find(params[:id])
         if @banners.destroy
             render json: { json: 'Banner was successfully deleted.'}
         else
             render json: { json: @banners.errors, status: :unprocessable_entity }
         end
-        end
+    end
 
-        private
+    private
         def banners_params
             params.require(:banner).permit(:id, :name, :banner_type)
         end
--- app/controllers/api/customers_controller.rb
+++ app/controllers/api/customers_controller.rb
@@ -1,41 +1,41 @@
 module Api
     class CustomersController < ApplicationController
-    def index
-    # @customers = Customer.where(deleted_at: nil)
-    @customers = Customer.all
-    render json: @customers
+        def index
+        # @customers = Customer.where(deleted_at: nil)
+        @customers = Customer.all
+        render json: @customers
     end
 
     def create   
     @customers = Customer.new(customers_params)
-    if @customers.save
-        render json: @customers 
-    else
-        render json: { json: @customers.errors, status: :unprocessable_entity }
-    end
+        if @customers.save
+            render json: @customers 
+        else
+            render json: { json: @customers.errors, status: :unprocessable_entity }
+        end
     end
 
     def show
-    @customers = Customer.find(params[:id])
-    render json: { data: @customers, status: :ok, message: 'Success' }
+        @customers = Customer.find(params[:id])
+        render json: { data: @customers, status: :ok, message: 'Success' }
     end
 
     def update
-    @customers = Customer.find(params[:id])
-    if @customers.update(customers_params)
-        render json: { status: :ok, message: 'Success' }
-    else
-        render json: { json: @customers.error, status: :unprocessable_entity }
-    end
+        @customers = Customer.find(params[:id])
+        if @customers.update(customers_params)
+            render json: { status: :ok, message: 'Success' }
+        else
+            render json: { json: @customers.error, status: :unprocessable_entity }
+        end
     end
 
     def destroy
-    @customers = Customer.find(params[:id])
-    if @customers.destroy
-        render json: { json: 'Customer was successfully deleted.'}
-    else
-        render json 
-    end
+        @customers = Customer.find(params[:id])
+        if @customers.destroy
+            render json: { json: 'Customer was successfully deleted.'}
+        else
+            render json 
+        end
     end
     
     private
