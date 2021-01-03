edeee1ded30f7a2506221898c243f856142886e2
huuphuc.0261993@gmail.com <huuphuc.0261993@gmail.com>
2021-01-03 15:09:45 +0700

    upload

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
+  host = '103.221.220.21' #replace with your own url
+  config.action_mailer.default_url_options =  { :host => '103.221.220.21', protocol: 'http' }
+  
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "customer.aphue@gmail.com",
+    :password             => "awwepqryiimhsuhr",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
