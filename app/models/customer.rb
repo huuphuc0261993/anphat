9ee8d537b9646fc3dce486e3e9857518c41b7518
huuphuc.0261993@gmail.com <huuphuc.0261993@gmail.com>
2021-01-06 09:38:56 +0700

    destroy

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :orders
+  has_many :orders, dependent: :destroy
   devise :database_authenticatable, :registerable, :recoverable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
